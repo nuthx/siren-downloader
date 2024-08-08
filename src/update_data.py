db609de88cbad4055699f45614d08a7186e4587c
NuthX <huermos@gmail.com>
2024-08-08 14:10:20 +0800

    feat: 设置新增跳过专辑与歌曲更新

--- src/update_data.py
+++ src/update_data.py
@@ -1,6 +1,7 @@
 import os
 import json
 
+from src.load_config import load_config
 from src.fetch_data import fetch_album_data, fetch_song_data
 
 
@@ -12,13 +13,15 @@ def update_data(song_list):
     else:
         song_list_local = song_list
 
-    # 更新全部
-    print(f"更新专辑数据")
-    fetch_album_data(song_list_local)
+    # 更新专辑数据：全部
+    if not load_config("debug", "skip_album_update", bool=True):
+        print(f"更新专辑数据")
+        fetch_album_data(song_list_local)
 
-    # 只更新需要下载的
-    print("更新歌曲数据")
-    fetch_song_data(song_list_local)
+    # 更新歌曲数据：仅需要下载
+    if not load_config("debug", "skip_song_update", bool=True):
+        print("更新歌曲数据")
+        fetch_song_data(song_list_local)
 
     print("保存数据到本地")
     with open("conf/data.json", "w", encoding="utf-8") as json_file:
