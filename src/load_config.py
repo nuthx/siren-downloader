a1109ec7c8606b10f31cf85428f9a0facaeb6ee3
NuthX <huermos@gmail.com>
2024-08-10 20:21:23 +0800

    fix: 修复win下配置文件的编码问题

--- src/load_config.py
+++ src/load_config.py
@@ -3,7 +3,8 @@ import configparser
 
 def load_config(category, item, bool=False):
     config = configparser.ConfigParser()
-    config.read("conf/config.ini")
+    with open("conf/config.ini", "r", encoding="utf-8") as config_file:
+        config.read_file(config_file)
 
     if bool:
         return config.getboolean(category, item)
