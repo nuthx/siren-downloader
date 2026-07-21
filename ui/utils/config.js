946bcf9fb22937298a23c211688f097e81c0ec83
nuthx <29234438+nuthx@users.noreply.github.com>
2026-07-21 18:15:48 +0800

    chore: 更新lint

--- ui/utils/config.js
+++ ui/utils/config.js
@@ -12,16 +12,16 @@ const DEFAULT_CONFIG = {
 
 const SYSTEM_ALBUM_MATCH = {
   "Operation Barrenland": "Operation Barrenland (W&W Soundtrack Mix)",
-  危机合约黄铁·利刃·燃灰OST: "危机合约 黄铁·利刃·燃灰OST",
+  "危机合约黄铁·利刃·燃灰OST": "危机合约 黄铁·利刃·燃灰OST",
   "Operation Pine Soot": "危机合约松烟行动OST",
   "2022明日方舟音律联觉-灯下定影原声EP": "音律联觉-灯下定影原声EP",
   "8-bit弹雨与断罪之拳": "狂弹要塞！罗德大兵集结",
   "冉冉升起，直播新星": "最后的全能系美少女！主播U的每日泰拉分享 (真没跑路)",
-  冬隐归路OST: "明日方舟：冬隐归路OST",
+  "冬隐归路OST": "明日方舟：冬隐归路OST",
   "A Toda Vela": "出苍白海OST",
-  焰烬曙明OST: "明日方舟：焰烬曙明OST",
-  卫戍协议盟约OST: "卫戍协议：盟约OST",
-  涤墨作战OST: "危机合约涤墨作战OST",
+  "焰烬曙明OST": "明日方舟：焰烬曙明OST",
+  "卫戍协议盟约OST": "卫戍协议：盟约OST",
+  "涤墨作战OST": "危机合约涤墨作战OST",
 }
 
 let storeInstance = null
